f5624e05592248dce82606fa3df08b63d2e25c1f
kamaladdi <kamaladdi@users.noreply.github.com>
2025-10-30 02:41:41 +0000

    deploy: cffd713d95af5673ea5fd5a0b246f84d7cccd5f9

---
+++ chunk-6UL3ORUE.js
@@ -0,0 +1,1 @@
+import{g as a}from"./chunk-JNSU4IKB.js";import"./chunk-LY56X4X7.js";export{a as BrandDetailPageComponent};
---
+++ chunk-BIWG3CL5.js
@@ -0,0 +1,1 @@
+import{j as a}from"./chunk-HQ36IUH4.js";import"./chunk-LY56X4X7.js";export{a as BrandsPageComponent};
